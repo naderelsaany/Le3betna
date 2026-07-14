22024d19068d77621ef9996fdcd9a48b81c426ed
Nader Elsadany <nader.elsadany@example.com>
2026-07-14 21:57:55 +0300

    chore: add 301 redirect for old vercel domain

--- landing_page/next.config.ts
+++ landing_page/next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'le3betna.vercel.app',
+          },
+        ],
+        destination: 'https://le3betna.cc.cd/:path*',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
